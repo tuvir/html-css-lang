56b123c81800c6e86a581bb26669871810fc8611
tuvir <kpetr03job@mail.ru>
2022-11-07 10:30:18 +0600

    Made a working navigation with smooth scroll

--- 08-site-optimization/js/script.js
+++ 08-site-optimization/js/script.js
@@ -17,6 +17,33 @@ const headerEl = document.querySelector(".main-header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// making smooth scroll animation
+const allLinksEl = document.querySelectorAll("a:link");
+
+allLinksEl.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    //scroll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //scrolll to sections
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    //closing mobile navigation
+    if (link.classList.contains("nav-element")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
